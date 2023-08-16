acf964331f9d36dd542101195eb7c854bd57226c
MrDave <davidschnoll@gmail.com>
2023-08-16 20:15:16 +0300

    Get a specific list of jobs (programmer in Moscow)

--- superjob.py
+++ superjob.py
@@ -1,4 +1,5 @@
 from environs import Env
+from pprint import pprint
 import requests
 
 
@@ -10,14 +11,27 @@ def get_sj_vacancies(secret_key: str):
     }
 
     params = {
-
+        "catalogues": 48,
+        "town": 4
     }
+
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+
+    return response
 
 
 def main():
-    env = Env
+    env = Env()
     env.read_env()
 
     sj_key = env.str("SJ_KEY")
+    vacancies = get_sj_vacancies(sj_key)
 
+    for vacancy in vacancies.json()["objects"]:
+        print(vacancy["profession"], vacancy["town"]["title"], sep=", ")
+
+
+if __name__ == '__main__':
+    main()
 
